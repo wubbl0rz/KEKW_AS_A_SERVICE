41a7576e1310746b65a30adc17a184e807cb4710
wubbl0rz <wubbl0rz@gmx.de>
2021-08-08 19:12:06 +0200

    ansible ist PogU

---
+++ KEKW_AS_A_SERVICE/Controllers/KEKWController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KEKW_AS_A_SERVICE.Controllers
+{
+  [ApiController]
+  [Route("/")]
+  public class KEKWController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      var random = new Random();
+      var n = random.Next(0, 360);
+      
+      var file = System.IO.File.OpenRead($"wwwroot/out{n}.png");
+
+      return File(file, "image/png");
+    }
+  }
+}
